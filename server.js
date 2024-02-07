13cbe58b28b51e9920c3dc95828581e4d8dc3039
joao.mateus <joao.mateus@softplan.com.br>
2024-02-07 10:36:46 -0300

    Audited Backend

--- server.js
+++ server.js
@@ -1,48 +1,55 @@
 //C:\next-news-project\backend\server.js
-require('dotenv').config(); // Importa e configura as variáveis de ambiente do arquivo .env
-const express = require('express'); // Importa o framework Express
-const mongoose = require('mongoose'); // Importa o ODM Mongoose para conexão com o MongoDB
-const bodyParser = require('body-parser'); // Middleware para análise de corpos de requisição
-const cors = require('cors'); // Middleware para habilitar requisições cross-origin
-const multer = require('multer'); // Middleware para manipulação de uploads de arquivos
-const { S3Client, GetObjectCommand } = require("@aws-sdk/client-s3"); // Importa o cliente S3 do AWS SDK
-const path = require('path'); // Importa o módulo path para manipulação de caminhos de arquivos
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const multer = require('multer');
 
-// Importa os modelos e rotas
+// Modelos e rotas
 const Article = require('./models/article');
 const articleRouter = require('./routes/articles');
 const authRouter = require('./routes/auth');
 const deleteRouter = require('./routes/delete');
 const authenticateToken = require('./routes/authMiddleware');
+const path = require('path');
+const { S3Client, GetObjectCommand } = require("@aws-sdk/client-s3");
 
-const app = express(); // Cria uma instância do aplicativo Express
+
+
+const app = express();
 
 // Configuração do Multer para uploads de arquivos
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads/'); // Define o diretório de destino para os uploads de arquivos
+    cb(null, 'uploads/'); // Certifique-se de que a pasta 'uploads' exista
   },
   filename: function (req, file, cb) {
-    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname)); // Define o nome do arquivo
+    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
   }
 });
-const upload = multer({ storage: storage }); // Configura o middleware Multer com a estratégia de armazenamento
+const upload = multer({ storage: storage });
 
 app.use(cors({
-  origin: 'https://portaldenoticiasnext.vercel.app' // Define as origens permitidas para requisições cross-origin
+  origin: 'https://portaldenoticiasnext.vercel.app' // Permitir requisições do seu domínio frontend
 }));
-app.use(bodyParser.json()); // Configura o middleware bodyParser para análise de corpos de requisição JSON
-app.use('/uploads', express.static('uploads')); // Define o diretório 'uploads' como estático para servir arquivos
+app.use(bodyParser.json());
+app.use('/uploads', express.static('uploads')); // Servir uploads de arquivos estáticos
 
-// Conexão com o MongoDB usando as credenciais do arquivo .env
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB Connected...')) // Log de sucesso na conexão com o MongoDB
-  .catch(err => console.error('MongoDB connection error:', err)); // Log de erro na conexão com o MongoDB
+// Conexão com o MongoDB
+mongoose.connect('mongodb+srv://testedb:batata123@cluster0.hcqoubl.mongodb.net/myDatabaseName?retryWrites=true&w=majority')
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+// Rotas públicas (sem autenticação)
+app.use('/api/articles', articleRouter); // Rota para obter artigos
+
+
 
 // Rota para servir imagens do S3 com chave dinâmica
 app.get('/s3-images/:key', async (req, res) => {
-  const key = req.params.key; // Obtém a chave dinâmica dos parâmetros da requisição
-  const s3 = new S3Client({ // Cria uma nova instância do cliente S3 com as credenciais do arquivo .env
+  const key = req.params.key;
+  const s3 = new S3Client({
     region: process.env.AWS_REGION,
     credentials: {
       accessKeyId: process.env.AWS_ACCESS_KEY_ID,
@@ -50,48 +57,46 @@ app.get('/s3-images/:key', async (req, res) => {
     }
   });
 
-  const getObjectParams = { // Parâmetros para obter o objeto (imagem) do S3
-    Bucket: 'nextnewsproject', // Nome do bucket
-    Key: key // Chave do objeto (imagem)
+  const getObjectParams = {
+    Bucket: 'nextnewsproject', // Substitua pelo nome do seu bucket
+    Key: key // A chave (Key) é dinâmica com base na notícia
   };
 
   try {
-    const { Body } = await s3.send(new GetObjectCommand(getObjectParams)); // Obtém o corpo (conteúdo) da imagem do S3
-    res.setHeader('Content-Type', 'image/jpeg'); // Define o tipo de conteúdo como imagem JPEG
-    Body.pipe(res); // Envia o corpo (conteúdo) da imagem como resposta
+    const { Body } = await s3.send(new GetObjectCommand(getObjectParams));
+    res.setHeader('Content-Type', 'image/jpeg'); // Defina o tipo de conteúdo apropriado
+    Body.pipe(res);
   } catch (err) {
-    console.error(err); // Log de erro caso a imagem não seja encontrada
-    res.status(404).send('Imagem não encontrada'); // Retorna status 404 se a imagem não for encontrada
+    console.error(err);
+    res.status(404).send('Imagem não encontrada');
   }
 });
-
-// Rotas públicas (sem autenticação)
-app.use('/api/articles', articleRouter); // Rota para obter artigos
 
 // Rotas protegidas (com autenticação)
 app.use('/api/admin/articles', authenticateToken, articleRouter); // Rota para criar/deletar/editar artigos
 app.use('/api/admin/delete', authenticateToken, deleteRouter); // Rota para ações administrativas adicionais
 
+
 // Rota para páginas individuais de notícias
 app.get('/article/:slug', async (req, res) => {
-  const slug = req.params.slug; // Obtém o slug dos parâmetros da requisição
+  const slug = req.params.slug;
   try {
-    const article = await Article.findOne({ slug }); // Busca o artigo pelo slug no MongoDB
-    if (!article) { // Se o artigo não for encontrado, retorna status 404 com uma mensagem JSON
+    const article = await Article.findOne({ slug });
+    if (!article) {
       return res.status(404).json({ message: 'Notícia não encontrada' });
     }
-    res.json(article); // Se o artigo for encontrado, retorna o artigo como JSON
+    res.json(article);
   } catch (err) {
-    console.error(err); // Log de erro caso ocorra um erro interno do servidor
-    res.status(500).json({ message: 'Erro interno do servidor' }); // Retorna status 500 em caso de erro interno do servidor
+    console.error(err);
+    res.status(500).json({ message: 'Erro interno do servidor' });
   }
 });
 
 // Rotas de autenticação
 app.use('/api/auth', authRouter);
 
-// Iniciar o servidor na porta definida no arquivo .env ou na porta 5000
+// Iniciar o servidor
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`); // Log de sucesso na inicialização do servidor
+  console.log(`Server running on port ${PORT}`);
 });
